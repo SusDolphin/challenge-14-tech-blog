af6efd5e72094e1c9bf173a51dbfd1c88c86151d
Larry Fletcher <susdolphin@Larrys-Laptop.local>
2025-01-19 00:49:51 -0500

    some changes to models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
 
+// User-Project relationships
 User.hasMany(Project, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -10,4 +12,22 @@ Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Project.hasMany(Comment, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Project, {
+  foreignKey: 'project_id'
+});
+
+module.exports = { User, Project, Comment };
